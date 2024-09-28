e8a1acb324583f655178322578b559e04bb52e08
unknown <rafik.towati@gmail.com>
2024-09-28 10:25:57 +0300

    cond motor ++++++

---
+++ previleges.php
@@ -0,0 +1,819 @@
+<?php
+
+$tab_info = array(
+  13453 =>
+  array(
+    'name' => 'employee',
+  ),
+  13462 =>
+  array(
+    'name' => 'hrm_leave',
+  ),
+  13463 =>
+  array(
+    'name' => 'hrm_leave_type',
+  ),
+  13684 =>
+  array(
+    'name' => 'orgunit',
+  ),
+  13685 =>
+  array(
+    'name' => 'orgunit_type',
+  ),
+  3630 =>
+  array(
+    'name' => 'practice_owner',
+  ),
+  3629 =>
+  array(
+    'name' => 'relation_ship',
+  ),
+  3631 =>
+  array(
+    'name' => 'user_owner',
+  ),
+);
+$tbf_info = array(
+  'employee' =>
+  array(
+    'id' => '13453',
+    'display' =>
+    array(
+      'id' => '103208',
+    ),
+    'search' =>
+    array(
+      'id' => '103209',
+    ),
+    'qsearch' =>
+    array(
+      'id' => '103210',
+    ),
+    'edit' =>
+    array(
+      'id' => '103205',
+    ),
+    'qedit' =>
+    array(
+      'id' => '103206',
+    ),
+    'crossed' =>
+    array(
+      'id' => -1,
+    ),
+    'stats' =>
+    array(
+      'id' => '103211',
+    ),
+    'ddb' =>
+    array(
+      'id' => -1,
+    ),
+    'minibox' =>
+    array(
+      'id' => -1,
+    ),
+  ),
+  'hrm_leave' =>
+  array(
+    'id' => '13462',
+    'display' =>
+    array(
+      'id' => '103198',
+    ),
+    'search' =>
+    array(
+      'id' => '103199',
+    ),
+    'qsearch' =>
+    array(
+      'id' => '103200',
+    ),
+    'edit' =>
+    array(
+      'id' => '103195',
+    ),
+    'qedit' =>
+    array(
+      'id' => '103196',
+    ),
+    'crossed' =>
+    array(
+      'id' => -1,
+    ),
+    'stats' =>
+    array(
+      'id' => '103201',
+    ),
+    'ddb' =>
+    array(
+      'id' => -1,
+    ),
+    'minibox' =>
+    array(
+      'id' => -1,
+    ),
+  ),
+  'hrm_leave_type' =>
+  array(
+    'id' => '13463',
+    'display' =>
+    array(
+      'id' => '103204',
+    ),
+    'search' =>
+    array(
+      'id' => -1,
+    ),
+    'qsearch' =>
+    array(
+      'id' => -1,
+    ),
+    'edit' =>
+    array(
+      'id' => -1,
+    ),
+    'qedit' =>
+    array(
+      'id' => '103202',
+    ),
+    'crossed' =>
+    array(
+      'id' => -1,
+    ),
+    'stats' =>
+    array(
+      'id' => -1,
+    ),
+    'ddb' =>
+    array(
+      'id' => -1,
+    ),
+    'minibox' =>
+    array(
+      'id' => -1,
+    ),
+  ),
+  'orgunit' =>
+  array(
+    'id' => '13684',
+    'display' =>
+    array(
+      'id' => '103188',
+    ),
+    'search' =>
+    array(
+      'id' => '103189',
+    ),
+    'qsearch' =>
+    array(
+      'id' => '103190',
+    ),
+    'edit' =>
+    array(
+      'id' => '103185',
+    ),
+    'qedit' =>
+    array(
+      'id' => '103186',
+    ),
+    'crossed' =>
+    array(
+      'id' => -1,
+    ),
+    'stats' =>
+    array(
+      'id' => '103191',
+    ),
+    'ddb' =>
+    array(
+      'id' => -1,
+    ),
+    'minibox' =>
+    array(
+      'id' => -1,
+    ),
+  ),
+  'orgunit_type' =>
+  array(
+    'id' => '13685',
+    'display' =>
+    array(
+      'id' => '103194',
+    ),
+    'search' =>
+    array(
+      'id' => -1,
+    ),
+    'qsearch' =>
+    array(
+      'id' => -1,
+    ),
+    'edit' =>
+    array(
+      'id' => -1,
+    ),
+    'qedit' =>
+    array(
+      'id' => '103192',
+    ),
+    'crossed' =>
+    array(
+      'id' => -1,
+    ),
+    'stats' =>
+    array(
+      'id' => -1,
+    ),
+    'ddb' =>
+    array(
+      'id' => -1,
+    ),
+    'minibox' =>
+    array(
+      'id' => -1,
+    ),
+  ),
+  'practice_owner' =>
+  array(
+    'id' => '3630',
+    'display' =>
+    array(
+      'id' => -1,
+    ),
+    'search' =>
+    array(
+      'id' => -1,
+    ),
+    'qsearch' =>
+    array(
+      'id' => -1,
+    ),
+    'edit' =>
+    array(
+      'id' => -1,
+    ),
+    'qedit' =>
+    array(
+      'id' => -1,
+    ),
+    'crossed' =>
+    array(
+      'id' => -1,
+    ),
+    'stats' =>
+    array(
+      'id' => -1,
+    ),
+    'ddb' =>
+    array(
+      'id' => -1,
+    ),
+    'minibox' =>
+    array(
+      'id' => -1,
+    ),
+  ),
+  'relation_ship' =>
+  array(
+    'id' => '3629',
+    'display' =>
+    array(
+      'id' => -1,
+    ),
+    'search' =>
+    array(
+      'id' => -1,
+    ),
+    'qsearch' =>
+    array(
+      'id' => -1,
+    ),
+    'edit' =>
+    array(
+      'id' => -1,
+    ),
+    'qedit' =>
+    array(
+      'id' => -1,
+    ),
+    'crossed' =>
+    array(
+      'id' => -1,
+    ),
+    'stats' =>
+    array(
+      'id' => -1,
+    ),
+    'ddb' =>
+    array(
+      'id' => -1,
+    ),
+    'minibox' =>
+    array(
+      'id' => -1,
+    ),
+  ),
+  'user_owner' =>
+  array(
+    'id' => '3631',
+    'display' =>
+    array(
+      'id' => -1,
+    ),
+    'search' =>
+    array(
+      'id' => -1,
+    ),
+    'qsearch' =>
+    array(
+      'id' => -1,
+    ),
+    'edit' =>
+    array(
+      'id' => -1,
+    ),
+    'qedit' =>
+    array(
+      'id' => -1,
+    ),
+    'crossed' =>
+    array(
+      'id' => -1,
+    ),
+    'stats' =>
+    array(
+      'id' => -1,
+    ),
+    'ddb' =>
+    array(
+      'id' => -1,
+    ),
+    'minibox' =>
+    array(
+      'id' => -1,
+    ),
+  ),
+);
+$role_info = array(
+  113 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'إدارة الموارد البشرية',
+      'en' => 'HRM',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '113',
+      'menu_name' => 'إدارة الموارد البشرية',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=113',
+      'css' => 'info',
+      'icon' => ' icon-113',
+      'showme' => true,
+      'items' =>
+      array(),
+      'sub-folders' =>
+      array(
+        60 =>
+        array(
+          'need_admin' => false,
+          'id' => '60',
+          'menu_name' => 'الحضور والغياب',
+          'page' => 'main.php?Main_Page=fm.php&a=1072&r=60',
+          'css' => 'info',
+          'icon' => ' icon-60',
+          'showme' => true,
+          'items' =>
+          array(),
+          'sub-folders' =>
+          array(),
+        ),
+        62 =>
+        array(
+          'need_admin' => false,
+          'id' => '62',
+          'menu_name' => 'بيانات الموظفين',
+          'page' => 'main.php?Main_Page=fm.php&a=1072&r=62',
+          'css' => 'info',
+          'icon' => ' icon-62',
+          'showme' => true,
+          'items' =>
+          array(),
+          'sub-folders' =>
+          array(),
+        ),
+      ),
+    ),
+  ),
+  275 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'إدارة الهيكل التنظيمي',
+      'en' => 'arole.275',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '275',
+      'menu_name' => 'إدارة الهيكل التنظيمي',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=275',
+      'css' => 'info',
+      'icon' => ' icon-275',
+      'showme' => true,
+      'items' =>
+      array(
+        103185 =>
+        array(
+          'id' => '103185',
+          'menu_name' => 'إنشاء منشأة/فرع',
+          'page' => 'main.php?Main_Page=afw_mode_edit.php&cl=Orgunit&currmod=hrm',
+          'css' => 'bf',
+          'icon' => 'edit',
+        ),
+        103190 =>
+        array(
+          'id' => '103190',
+          'menu_name' => 'الاستعلام عن منشأة/فرع',
+          'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=Orgunit&currmod=hrm',
+          'css' => 'bf',
+          'icon' => 'search',
+        ),
+      ),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+  274 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'الاستعلامات',
+      'en' => 'arole.274',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '274',
+      'menu_name' => 'الاستعلامات',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=274',
+      'css' => 'info',
+      'icon' => ' icon-274',
+      'showme' => true,
+      'items' =>
+      array(
+        103210 =>
+        array(
+          'id' => '103210',
+          'menu_name' => 'الاستعلام عن موظف',
+          'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=Employee&currmod=hrm',
+          'css' => 'bf',
+          'icon' => 'search',
+        ),
+        103185 =>
+        array(
+          'id' => '103185',
+          'menu_name' => 'إنشاء منشأة/فرع',
+          'page' => 'main.php?Main_Page=afw_mode_edit.php&cl=Orgunit&currmod=hrm',
+          'css' => 'bf',
+          'icon' => 'edit',
+        ),
+        103190 =>
+        array(
+          'id' => '103190',
+          'menu_name' => 'الاستعلام عن منشأة/فرع',
+          'page' => 'main.php?Main_Page=afw_mode_qsearch.php&cl=Orgunit&currmod=hrm',
+          'css' => 'bf',
+          'icon' => 'search',
+        ),
+      ),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+  263 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'البيانات المرجعية',
+      'en' => 'Lookup data',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '263',
+      'menu_name' => 'البيانات المرجعية',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=263',
+      'css' => 'info',
+      'icon' => ' icon-263',
+      'showme' => true,
+      'items' =>
+      array(),
+      'sub-folders' =>
+      array(
+        265 =>
+        array(
+          'need_admin' => false,
+          'id' => '265',
+          'menu_name' => 'الحضور والغياب',
+          'page' => 'main.php?Main_Page=fm.php&a=1072&r=265',
+          'css' => 'info',
+          'icon' => ' icon-265',
+          'showme' => true,
+          'items' =>
+          array(
+            103202 =>
+            array(
+              'id' => '103202',
+              'menu_name' => 'إدارة أنواع الاجازات والأذونات',
+              'page' => 'main.php?Main_Page=afw_mode_qedit.php&cl=HrmLeaveType&currmod=hrm&ids=all&newo=3',
+              'css' => 'bf',
+              'icon' => 'edit',
+            ),
+          ),
+          'sub-folders' =>
+          array(),
+        ),
+        267 =>
+        array(
+          'need_admin' => false,
+          'id' => '267',
+          'menu_name' => 'الرواتب',
+          'page' => 'main.php?Main_Page=fm.php&a=1072&r=267',
+          'css' => 'info',
+          'icon' => ' icon-267',
+          'showme' => true,
+          'items' =>
+          array(),
+          'sub-folders' =>
+          array(),
+        ),
+        264 =>
+        array(
+          'need_admin' => false,
+          'id' => '264',
+          'menu_name' => 'النصوص',
+          'page' => 'main.php?Main_Page=fm.php&a=1072&r=264',
+          'css' => 'info',
+          'icon' => ' icon-264',
+          'showme' => true,
+          'items' =>
+          array(),
+          'sub-folders' =>
+          array(),
+        ),
+        266 =>
+        array(
+          'need_admin' => false,
+          'id' => '266',
+          'menu_name' => 'بيانات الموظفين',
+          'page' => 'main.php?Main_Page=fm.php&a=1072&r=266',
+          'css' => 'info',
+          'icon' => ' icon-266',
+          'showme' => true,
+          'items' =>
+          array(
+            103192 =>
+            array(
+              'id' => '103192',
+              'menu_name' => 'إدارة أنواع عنصر الهيكل التنظيمي',
+              'page' => 'main.php?Main_Page=afw_mode_qedit.php&cl=OrgunitType&currmod=hrm&ids=all&newo=3',
+              'css' => 'bf',
+              'icon' => 'edit',
+            ),
+          ),
+          'sub-folders' =>
+          array(),
+        ),
+      ),
+    ),
+  ),
+  60 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'الحضور والغياب',
+      'en' => 'Module-1075',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '60',
+      'menu_name' => 'الحضور والغياب',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=60',
+      'css' => 'info',
+      'icon' => ' icon-60',
+      'showme' => true,
+      'items' =>
+      array(),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+  265 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'الحضور والغياب',
+      'en' => 'Hrm-module-1075',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '265',
+      'menu_name' => 'الحضور والغياب',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=265',
+      'css' => 'info',
+      'icon' => ' icon-265',
+      'showme' => true,
+      'items' =>
+      array(
+        103202 =>
+        array(
+          'id' => '103202',
+          'menu_name' => 'إدارة أنواع الاجازات والأذونات',
+          'page' => 'main.php?Main_Page=afw_mode_qedit.php&cl=HrmLeaveType&currmod=hrm&ids=all&newo=3',
+          'css' => 'bf',
+          'icon' => 'edit',
+        ),
+      ),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+  267 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'الرواتب',
+      'en' => 'Hrm-module-1077',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '267',
+      'menu_name' => 'الرواتب',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=267',
+      'css' => 'info',
+      'icon' => ' icon-267',
+      'showme' => true,
+      'items' =>
+      array(),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+  264 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'النصوص',
+      'en' => 'Texts',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '264',
+      'menu_name' => 'النصوص',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=264',
+      'css' => 'info',
+      'icon' => ' icon-264',
+      'showme' => true,
+      'items' =>
+      array(),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+  62 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'بيانات الموظفين',
+      'en' => 'Module-1076',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '62',
+      'menu_name' => 'بيانات الموظفين',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=62',
+      'css' => 'info',
+      'icon' => ' icon-62',
+      'showme' => true,
+      'items' =>
+      array(),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+  266 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'بيانات الموظفين',
+      'en' => 'Hrm-module-1076',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '266',
+      'menu_name' => 'بيانات الموظفين',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=266',
+      'css' => 'info',
+      'icon' => ' icon-266',
+      'showme' => true,
+      'items' =>
+      array(
+        103192 =>
+        array(
+          'id' => '103192',
+          'menu_name' => 'إدارة أنواع عنصر الهيكل التنظيمي',
+          'page' => 'main.php?Main_Page=afw_mode_qedit.php&cl=OrgunitType&currmod=hrm&ids=all&newo=3',
+          'css' => 'bf',
+          'icon' => 'edit',
+        ),
+      ),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+  273 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'بيانات الموظفين',
+      'en' => 'arole.273',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '273',
+      'menu_name' => 'بيانات الموظفين',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=273',
+      'css' => 'info',
+      'icon' => ' icon-273',
+      'showme' => true,
+      'items' =>
+      array(
+        103205 =>
+        array(
+          'id' => '103205',
+          'menu_name' => 'إنشاء موظف',
+          'page' => 'main.php?Main_Page=afw_mode_edit.php&cl=Employee&currmod=hrm',
+          'css' => 'bf',
+          'icon' => 'edit',
+        ),
+      ),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+  192 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'طلبات الدعم الفني',
+      'en' => 'Tickets',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '192',
+      'menu_name' => 'طلبات الدعم الفني',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=192',
+      'css' => 'info',
+      'icon' => ' icon-192',
+      'showme' => true,
+      'items' =>
+      array(),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+  269 =>
+  array(
+    'name' =>
+    array(
+      'ar' => 'طلبات الموظفين',
+      'en' => 'arole.269',
+    ),
+    'menu' =>
+    array(
+      'need_admin' => false,
+      'id' => '269',
+      'menu_name' => 'طلبات الموظفين',
+      'page' => 'main.php?Main_Page=fm.php&a=1072&r=269',
+      'css' => 'info',
+      'icon' => ' icon-269',
+      'showme' => true,
+      'items' =>
+      array(),
+      'sub-folders' =>
+      array(),
+    ),
+  ),
+);
