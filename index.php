33779e46d7798aa0012d494bd1c85e1264fa94a8
unknown <rafik.towati@gmail.com>
2024-12-16 09:06:22 +0300

    bug fix + some contents

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
-$file_dir_name = dirname(__FILE__);
-
-$Direct_Page = "work.php";
-include("$file_dir_name/../lib/afw/afw_direct_page.php");
+$direct_dir_name = $file_dir_name = dirname(__FILE__);
+include("$file_dir_name/hrm_start.php");
+$objme = AfwSession::getUserConnected();
+if($objme)
+{
+    $Main_Page = "work.php";
+    $MODULE = $My_Module = "hrm";
+    $options = [];
+    $options["dashboard-stats"] = true;
+    $options["chart-js"] = true;
+    AfwMainPage::echoMainPage($My_Module, $Main_Page, $file_dir_name, $options);
+}
 
 ?>
