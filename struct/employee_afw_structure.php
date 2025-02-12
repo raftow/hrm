c89f0d7e308511c961e63acc70f80a1d49f0f181
raftow <rboubaker@tvtc.gov.sa>
2025-02-12 16:56:38 +0300

    %comment%

--- struct/employee_afw_structure.php
+++ struct/employee_afw_structure.php
@@ -903,8 +903,7 @@ class HrmEmployeeAfwStructure
 			'SHOW' => true,
 			'RETRIEVE' => false,
 			'QEDIT' => false,
-			'TYPE' => 'FK',
-			'ANSWER' => 'scenario_item',
+			'TYPE' => 'INT', /*stepnum-not-the-object*/
 			'ANSMODULE' => 'ums',
 			'FGROUP' => 'tech_fields'
 		),
