f301b6701967048f537143488d2822673428ec7b
rafiq boubaker <rafik.towati@gmail.com>
2025-08-07 04:39:18 +0300

    work on uoh deployment from Tunis

--- work.php
+++ work.php
@@ -2,8 +2,8 @@
 $file_dir_name = dirname(__FILE__);
 $extended_orgunit_class="Orgunit";
 $extended_orgunit_module="hrm";
-include("$file_dir_name/../hrm/org.php");
-
+// include("$file_dir_name/../hrm/org.php");
+$out_scr = "to implement";
 echo $out_scr;
    
 ?>
